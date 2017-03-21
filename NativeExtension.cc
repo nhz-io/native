56a307139940d393540ab4d3449ff96934243ab3
Ishi Ruy <dev@nhz.io>
2017-03-21 20:34:00 +0200

    reindent

--- NativeExtension.cc
+++ NativeExtension.cc
@@ -1,28 +1,78 @@
+// Original: https://github.com/fcanas/node-native-boilerplate/blob/master/functions.cc
+
 #include "functions.h"
 
-using v8::FunctionTemplate;
-
-// NativeExtension.cc represents the top level of the module.
-// C++ constructs that are exposed to javascript are exported here
-
-NAN_MODULE_INIT(InitAll) {
-  Nan::Set(target, Nan::New("nothing").ToLocalChecked(),
-    Nan::GetFunction(Nan::New<FunctionTemplate>(nothing)).ToLocalChecked());
-  Nan::Set(target, Nan::New("aString").ToLocalChecked(),
-    Nan::GetFunction(Nan::New<FunctionTemplate>(aString)).ToLocalChecked());
-  Nan::Set(target, Nan::New("aBoolean").ToLocalChecked(),
-    Nan::GetFunction(Nan::New<FunctionTemplate>(aBoolean)).ToLocalChecked());
-  Nan::Set(target, Nan::New("aNumber").ToLocalChecked(),
-    Nan::GetFunction(Nan::New<FunctionTemplate>(aNumber)).ToLocalChecked());
-  Nan::Set(target, Nan::New("anObject").ToLocalChecked(),
-    Nan::GetFunction(Nan::New<FunctionTemplate>(anObject)).ToLocalChecked());
-  Nan::Set(target, Nan::New("anArray").ToLocalChecked(),
-    Nan::GetFunction(Nan::New<FunctionTemplate>(anArray)).ToLocalChecked());
-  Nan::Set(target, Nan::New("callback").ToLocalChecked(),
-    Nan::GetFunction(Nan::New<FunctionTemplate>(callback)).ToLocalChecked());
-  
-  // Passing target down to the next NAN_MODULE_INIT
-  MyObject::Init(target);
+NAN_METHOD(nothing) {
 }
 
-NODE_MODULE(NativeExtension, InitAll)
+NAN_METHOD(aString) {
+    info.GetReturnValue().Set(Nan::New("This is a thing.").ToLocalChecked());
+}
+
+NAN_METHOD(aBoolean) {
+    info.GetReturnValue().Set(false);
+}
+
+NAN_METHOD(aNumber) {
+    info.GetReturnValue().Set(1.75);
+}
+
+NAN_METHOD(anObject) {
+    v8::Local<v8::Object> obj = Nan::New<v8::Object>();
+    Nan::Set(obj, Nan::New("key").ToLocalChecked(), Nan::New("value").ToLocalChecked());
+    info.GetReturnValue().Set(obj);
+}
+
+NAN_METHOD(anArray) {
+    v8::Local<v8::Array> arr = Nan::New<v8::Array>(3);
+    Nan::Set(arr, 0, Nan::New(1));
+    Nan::Set(arr, 1, Nan::New(2));
+    Nan::Set(arr, 2, Nan::New(3));
+    info.GetReturnValue().Set(arr);
+}
+
+NAN_METHOD(callback) {
+    v8::Local<v8::Function> callbackHandle = info[0].As<v8::Function>();
+    Nan::MakeCallback(Nan::GetCurrentContext()->Global(), callbackHandle, 0, 0);
+}
+
+// Wrapper Impl
+
+Nan::Persistent<v8::Function> MyObject::constructor;
+
+NAN_MODULE_INIT(MyObject::Init) {
+  v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
+  tpl->SetClassName(Nan::New("MyObject").ToLocalChecked());
+  tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+  Nan::SetPrototypeMethod(tpl, "plusOne", PlusOne);
+
+  constructor.Reset(Nan::GetFunction(tpl).ToLocalChecked());
+  Nan::Set(target, Nan::New("MyObject").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
+}
+
+MyObject::MyObject(double value) : value_(value) {
+}
+
+MyObject::~MyObject() {
+}
+
+NAN_METHOD(MyObject::New) {
+  if (info.IsConstructCall()) {
+    double value = info[0]->IsUndefined() ? 0 : Nan::To<double>(info[0]).FromJust();
+    MyObject *obj = new MyObject(value);
+    obj->Wrap(info.This());
+    info.GetReturnValue().Set(info.This());
+  } else {
+    const int argc = 1; 
+    v8::Local<v8::Value> argv[argc] = {info[0]};
+    v8::Local<v8::Function> cons = Nan::New(constructor);
+    info.GetReturnValue().Set(cons->NewInstance(argc, argv));
+  }
+}
+
+NAN_METHOD(MyObject::PlusOne) {
+  MyObject* obj = Nan::ObjectWrap::Unwrap<MyObject>(info.This());
+  obj->value_ += 1;
+  info.GetReturnValue().Set(obj->value_);
+}
